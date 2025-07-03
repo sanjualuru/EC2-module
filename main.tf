fef2b3ebcdb130349217bdb4578dc57e216c2f1c
sanjualuru <153225127+sanjualuru@users.noreply.github.com>
2025-07-03 19:49:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 resource "aws_instance" "intance1" {
 ami                         = var.ami_id
-  instance_type               = var.instance_type
-tags = {
-Name = var.instance_name 
-}
+instance_type               = var.instance_type
 }
